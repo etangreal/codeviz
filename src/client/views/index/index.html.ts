abb7ba3b5056d63c2a818e8af91fe7b9706e1d38
etangreal <etangreal@gmail.com>
2014-07-06 22:55:39 +0200

    Added Meteorite Package: typescript-libs | updated some docs/

--- src/client/views/index/index.html.ts
+++ src/client/views/index/index.html.ts
@@ -1,6 +1,7 @@
+
 // ----------------------------------------------------------------------------
 // REFERENCES
 // ----------------------------------------------------------------------------
+
 /// <reference path="../packages/typescript-libs/meteor.d.ts" />
 /// <reference path="../packages/typescript-libs/node.d.ts" />
-//# sourceMappingURL=index.html.js.map
