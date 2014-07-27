a546c8fdb3b2fa1d83af7a34d1f8694987fd86b0
etangreal <etangreal@gmail.com>
2014-07-27 16:45:32 +0200

    disabled call to phantomjs

--- src/methods/phantomTest.js
+++ src/methods/phantomTest.js
@@ -8,10 +8,10 @@ Meteor.methods({
 	phantomTest: function(id) {
 		if (Meteor.isClient) { console.log('client: phantomTest. id:', id); }
 
-		if (Meteor.isServer) {
-			console.log('server: CALLING => _phantomTest. id: ', id);
-			_phantomSpawnTest(id);
-		}
+		// if (Meteor.isServer) {
+		// 	console.log('server: CALLING => _phantomTest. id: ', id);
+		// 	_phantomSpawnTest(id);
+		// }
 	}
 
 });//Meteor.methods
