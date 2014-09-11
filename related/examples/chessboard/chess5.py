c78ced0fa3f3344a5a36366528bb9584b5fe0c4c
etangreal <etangreal@gmail.com>
2014-09-11 06:34:56 +0200

    sundry | Example

--- related/examples/chessboard/chess5.py
+++ related/examples/chessboard/chess5.py
@@ -1,5 +1,5 @@
 
-bitboard = []
+
 
 knight   	 	= long('0000000000000000000000000000000000010000000000000000000000000000', 2)
 moves  		 	= long('0000000000000000001010000100010000000000010001000010100000000000', 2)
@@ -8,6 +8,7 @@ knightPlusMoves = knight | moves
 whitePieces  	= long('0000000000000000000000000000000000010000000001000010000000000000', 2)
 knightCanMove 	= knightPlusMoves ^ whitePieces
 
+bitboard = []
 bitboard.append( knight )
 bitboard.append( moves ) 
 bitboard.append( knightPlusMoves )
